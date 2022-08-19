a59364b21c450ae3179edf3e9d4b4cac0faeed9a
Ahljenn <ahljennm2@gmail.com>
2022-08-18 18:11:25 -0700

    first commit

---
+++ inventory_management/inventory/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+class Device(models.Model):
+  type = models.CharField(max_length=100, blank=False)
+  issues = models.CharField(max_length=100, default='No issues')
+  description = models.TextField(max_length=100, blank=True)
+
+  status = models.CharField(
+    max_length = 20,
+    default = 'Sold',
+    choices = (
+      ('Sold', 'Sold'),
+      ('Available', 'Available'),
+      ('Restocking', 'Restocking'),
+    ))
+
+  price = models.PositiveIntegerField(default=1)
+
+  class Meta:
+    abstract = True #avoids making new models for this class 
+
+  def __str__(self):
+    '''Returns string representation of the Device model'''
+    return f'Type: {0} Price: {1}'.format(self.type, self.price)
+
+class Laptop(Device):
+  '''Inherits from the Device model'''
+  pass
+
+class Desktop(Device):
+  '''Inherits from the Device model'''
+  pass
+
+class Mobile(Device):
+  '''Inherits from the Device model'''
+  pass
