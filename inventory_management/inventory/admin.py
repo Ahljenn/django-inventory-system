5dfb723fc67f13d18bf7a86d5f3f3d2337265f57
Ahljenn <ahljennm2@gmail.com>
2022-08-19 23:19:13 -0700

    Added button functionality for interview

--- inventory_management/inventory/admin.py
+++ inventory_management/inventory/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Laptop)
-admin.site.register(Desktop)
-admin.site.register(Mobile)
+@admin.register(Desktop, Laptop, Mobile)
+class ViewAdmin(admin.ModelAdmin):
+  pass
--- inventory_management/inventory/urls.py
+++ inventory_management/inventory/urls.py
@@ -3,4 +3,7 @@ from .views import *
 
 urlpatterns = [
   re_path(r'^$', index, name='index'),
+  re_path(r'^display_desktops$', display_desktops, name='display_desktops'),
+  re_path(r'^display_laptops$', display_laptops, name='display_laptops'),
+  re_path(r'^display_mobiles$', display_mobiles, name='display_mobiles'),
 ]
