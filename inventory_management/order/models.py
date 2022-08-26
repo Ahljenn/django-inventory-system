7e3b51f8415d8dbfc7fd79dd23d2a0d27ac0432c
Ahljenn <ahljennm2@gmail.com>
2022-08-26 10:52:22 -0700

    Register model for order

--- inventory_management/order/models.py
+++ inventory_management/order/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+
+  type = models.CharField(max_length=100, blank=False)
+  price = models.PositiveIntegerField(default=1)
+
+  condition = models.CharField(
+    max_length=20,
+    default = 'New',
+    choices = (
+      ('New', 'New'),
+      ('Used', 'Used'),
+    ))
+
+  device_type = models.CharField(
+  max_length=20,
+  default='Desktop',
+  choices=(
+    ('Desktop', 'Desktop'),
+    ('Laptop', 'Laptop'),
+    ('Mobile', 'Mobile'),
+  ))
+
+  order_type = models.CharField(
+  max_length=20,
+  default='Order',
+  choices=(
+    ('Order', 'Order'),
+    ('Notify', 'Notify'),
+    ('Book', 'Book'),
+  ))
+
+  def __str__(self):
+    return f'{self.model} - ${self.price}'
